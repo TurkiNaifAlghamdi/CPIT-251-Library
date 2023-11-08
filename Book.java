2ca4e7731dba627c1e94d8a25f06f82978efca5f
Ali Zaid Hamidaddin <136197920+AZHamidaddin@users.noreply.github.com>
2023-11-09 00:22:57 +0300

    Added ISBN number to Book.java
    ISBN is like the ID of the book.

--- Book.java
+++ Book.java
@@ -1,8 +1,5 @@
-
-package library;
-
-
 public class Book {
+
     private String title;
     private String author;
     private int year;
@@ -10,10 +7,12 @@ public class Book {
     private String publisher;
     private int stock;
     private double price;
+    private long isbn;
     private Book right;
     private Book left;
-    // Constructor
-    public Book(String title, String author, int year, int location, String publisher, int stock, double price) {
+
+    // Constructors
+    public Book(String title, String author, int year, int location, String publisher, int stock, double price, long isbn) {
         this.title = title;
         this.author = author;
         this.year = year;
@@ -21,9 +20,20 @@ public class Book {
         this.publisher = publisher;
         this.stock = stock;
         this.price = price;
-        this.right = null;
-        this.left = null;
-        
+        this.isbn = isbn;
+    }
+
+    public Book(String title, String author, int year, int location, String publisher, int stock, double price, long isbn, Book right, Book left) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.location = location;
+        this.publisher = publisher;
+        this.stock = stock;
+        this.price = price;
+        this.isbn = isbn;
+        this.right = right;
+        this.left = left;
     }
 
     public Book getRight() {
@@ -41,63 +51,70 @@ public class Book {
     public void setLeft(Book left) {
         this.left = left;
     }
-    
-    
-    
+
     // Getters and Setters
     public String getTitle() {
         return title;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public String getAuthor() {
         return author;
     }
-    
+
     public void setAuthor(String author) {
         this.author = author;
     }
-    
+
     public int getYear() {
         return year;
     }
-    
+
     public void setYear(int year) {
         this.year = year;
     }
-    
+
     public int getLocation() {
         return location;
     }
-    
+
     public void setLocation(int location) {
         this.location = location;
     }
-    
+
     public String getPublisher() {
         return publisher;
     }
-    
+
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
-    
+
     public int getStock() {
         return stock;
     }
-    
+
     public void setStock(int stock) {
         this.stock = stock;
     }
-    
+
     public double getPrice() {
         return price;
     }
-    
+
     public void setPrice(double price) {
         this.price = price;
-    }    
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
 }
